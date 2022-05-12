5f82458a49d0764b436be9188bdde71b61410e13
sgold-KR <samuel.gold@kroger.com>
2022-05-12 14:08:07 -0600

    changed network

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_resource_group" "rg" {
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["192.168.4.0/24"]
   location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
 }
