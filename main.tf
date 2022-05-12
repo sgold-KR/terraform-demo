b2e06270e6f9f3c19f7427a7629de7ec275d4f5b
sgold-KR <samuel.gold@kroger.com>
2022-05-12 13:51:20 -0600

    adding terraform files

---
+++ main.tf
@@ -0,0 +1,41 @@
+# Configure the Azure provider
+terraform {
+   backend "azurerm" {
+      resource_group_name  = "myTFResourceGroup"
+      storage_account_name = "storageaccountsgdev"
+      container_name       = "blobcontainername"
+      key                  = "prod.terraform.tfstate"
+    }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+
+  tags = {
+      Environment = "Terraform Getting Started"
+      Team= "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
